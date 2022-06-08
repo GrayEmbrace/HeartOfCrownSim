a8da2534ab7931614475ccd48dc84b3d6193438c
GrayEmbrace <frostboy16@gmail.com>
2022-06-07 17:02:17 -0700

    moved bots to own files
    moved basicBot to its own file, and simplified bot turn arguments

---
+++ basicBot.py
@@ -0,0 +1,66 @@
+# Sample bot that implements a basic "Big Money" style strategy.
+
+## The field argument is a dict representing the current game state.
+## You can access the following:
+##      "hand": The player's hand, as a list.
+##      "deck": deck
+##      "discard": discard
+##      "domain": domain
+##      "points": points
+##      "deckPoints": deckPoints
+
+def botTurn(field):
+    log = ""
+    handGold = 0
+    handPoint = 0
+    field["hand"].sort()
+    for card in field["hand"]:
+        if card > 0:
+            handGold += card
+        elif card < 0:
+            handPoint += abs(card)
+    if len(field["domain"]) == 0:
+        if handGold >= 6:
+            log += "Backed Lulu\nDomain: "
+            field["points"] += 6
+            for domain in range(3):
+                card = field["hand"].pop()
+                field["domain"].append(card)
+                log += str(card)
+                if card == 1:
+                    field["points"] -= 2
+            log += "\n"
+        elif handGold >= 3:
+            log += "Bought: City\n"
+            field["discard"].append(2)
+    else:
+        if handPoint > 0:
+            log += "Scored: "
+            for card in field["hand"]:
+                if card < 0:
+                    match card:
+                        case -2:
+                            field["points"] += 2
+                            log += "Royal Maid "
+                        case -3:
+                            field["points"] += 3
+                            log += "Senator "
+                        case -6:
+                            field["points"] += 6
+                            log += "Duke "
+                    field["hand"].remove(card)
+            log += "\n"
+        else:
+            if handGold >= 8:
+                log += "Bought: Duke\n"
+                field["discard"].append(-6)
+                field["deckPoints"] += 6
+            elif handGold >= 5:
+                log += "Bought: Senator\n"
+                field["discard"].append(-3)
+                field["deckPoints"] += 3
+            elif handGold >= 3:
+                log += "Bought: Royal Maid\n"
+                field["discard"].append(-2)
+                field["deckPoints"] += 2
+    return [field, log]
--- hoc_sim.py
+++ hoc_sim.py
@@ -1,63 +1,7 @@
 import random
 # You will need to install matplotlib with pip to view coronation histograms!
 import matplotlib.pyplot as plt
-
-## Sample bot that implements a basic "Big Money" style strategy. In the future
-## this should be its own script to be imported, as well as optimizing the number
-## of arguments to pass.
-def botTurn(budget, handPoint, deckPoint, princessBacked):
-    scoreChange = 0
-    deckChange = 0
-    log = ""
-    if princessBacked != True:
-        if budget >= 6:
-            princessBacked = True
-            deckChange += 6
-            scoreChange += 6
-            log += "Backed Lulu\nDomain: "
-            for domain in range(3):
-                card = hand.pop()
-                log += str(card)
-                if card == 1:
-                    scoreChange -= 2
-            log += "\n"
-        elif budget >= 3:
-            log += "Bought: City\n"
-            discard.append(2)
-    else:
-        if handPoint < 0:
-            log += "Scored: "
-            for card in hand:
-                if card < 0:
-                    match card:
-                        case -2:
-                            scoreChange += 2
-                            log += "Royal Maid "
-                        case -3:
-                            scoreChange += 3
-                            log += "Senator "
-                        case -6:
-                            scoreChange += 6
-                            log += "Duke "
-                    hand.remove(card)
-            log += "\n"
-        else:
-            if budget >= 8:
-                deckChange += 6
-                discard.append(-6)
-                log += "Bought: Duke\n"
-            elif budget >= 6:
-                discard.append(3)
-                log += "Bought: Large City\n"
-            elif budget >= 5:
-                deckChange += 3
-                discard.append(-3)
-                log += "Bought: Senator\n"
-            elif budget >= 3:
-                deckChange += 2
-                discard.append(-2)
-                log += "Bought: Royal Maid\n"
-    return [princessBacked, scoreChange, deckChange, log]
+import basicBot
 
 ## This is a highly simplified version of Heart of Crown, where a card can only
 ## generate gold, be worth points, or neither. The convention used here is positive
@@ -67,7 +11,7 @@ def botTurn(budget, handPoint, deckPoint, princessBacked):
 ## each, and then create a histogram.
 turnCounts = []
 
-for iterations in range(1000):
+for iteration in range(1000):
     ## The standard starting deck has seven Farming Villages and three Apprentice
     ## Maids (represented as 0 since they are worth no gold and we have do not
     ## intend to use their -2 VP value.)
@@ -75,7 +19,7 @@ for iterations in range(1000):
     random.shuffle(deck)
     hand = []
     discard = []
-    princessBacked = False
+    domain = []
     points = 0
     turns = 0
     deckPoints = 0
@@ -91,20 +35,25 @@ for iterations in range(1000):
             hand.append(deck.pop())
         hand.sort()
         log += "Draw: {}\n".format(hand)
-        handMoney = 0
-        handPoint = 0
-        for card in hand:
-            if card > 0: handMoney += card
-            elif card < 0: handPoint += card
-        result = botTurn(handMoney, handPoint, deckPoints, princessBacked)
-        princessBacked = result[0]
-        points += result[1]
-        deckPoints += result[2]
-        log += result[3]
+        field = {
+            "hand": hand,
+            "deck": deck,
+            "discard": discard,
+            "domain": domain,
+            "points": points,
+            "deckPoints": deckPoints
+        }
+        result = basicBot.botTurn(field)
+        hand = result[0]["hand"]
+        deck = result[0]["deck"]
+        discard = result[0]["discard"]
+        domain = result[0]["domain"]
+        points = result[0]["points"]
+        deckPoints = result[0]["deckPoints"]
+        log += result[1]
         while len(hand) > 0:
             discard.append(hand.pop())
-    # View an arbitrary game log for debugging purposes.
-    if iterations == 15:
+    if iteration == 15:
         print(log)
     turnCounts.append(turns)
 
