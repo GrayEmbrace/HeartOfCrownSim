a2625c50a89aa5623812134581530490ea0887fb
GrayEmbrace <frostboy16@gmail.com>
2022-06-14 09:05:37 -0700

    added basic Klam Klam bot

---
+++ basicBot_klam.py
@@ -0,0 +1,57 @@
+# Basic Bot with Klam Klam.
+
+import cardLookup as cl
+
+def botTurn(field):
+    log = ""
+    handGold = 0
+    handPoint = 0
+    for card in field["hand"]:
+        handGold += cl.getGoldValue(card)
+        vpValue = cl.getVPValue(card)
+        if vpValue > 0: handPoint += vpValue
+    if len(field["domain"]) == 0:
+        if handGold >= 6:
+            log += "Backed Klam-Klam\nDomain: "
+            field["points"] += 6
+            field["hand"].sort(key=cl.getGoldValue)
+            for domain in range(3):
+                card = field["hand"].pop()
+                field["domain"].append(card)
+                log += str(card) + " "
+                if card == "Farming Village":
+                    field["points"] -= 2
+            log += "\n"
+        elif handGold >= 3:
+            log += "Bought: City\n"
+            field["discard"].append("City")
+    else:
+        if handPoint > 0:
+            log += "Scored: "
+            i = 0
+            while i < len(field["hand"]):
+                card = field["hand"][i]
+                vpValue = cl.getVPValue(card)
+                if cl.getVPValue(card) > 0:
+                    field["points"] += vpValue
+                    log += card + " "
+                    field["hand"].pop(i)
+                else:
+                    i += 1
+            log += "\n"
+        else:
+            if handGold >= 7:
+                log += "Bought: Duke\n"
+                field["discard"].append("Duke")
+                field["deckPoints"] += 6
+            elif handGold >= 4:
+                log += "Bought: Senator\n"
+                field["discard"].append("Senator")
+                field["deckPoints"] += 3
+            elif handGold >= 2:
+                log += "Bought: Royal Maid\n"
+                field["discard"].append("Royal Maid")
+                field["deckPoints"] += 2
+    log += "Points: " + str(field["points"]) + "\n"
+    return [field, log]
+
